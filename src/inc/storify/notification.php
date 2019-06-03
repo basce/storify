dbd8849a2b9d3a3eb3b3aae8aa1090339e2aa01c
Basce <basce.yong@gmail.com>
2019-06-03 18:10:58 +0800

    worker bot
    add worker bot file

--- src/inc/storify/notification.php
+++ src/inc/storify/notification.php
@@ -7,42 +7,62 @@ class notification{
 
 	}
 
-	public function addTask($name, $target_uid, $type, $data, $expected_time, $groupable){
+	public static function addTask($name, $target_uid, $type, $data, $expected_time, $groupable){
 		global $wpdb;
 
-		$query = "INSERT INTO `".$wpdb->prefix."task` ( name, notify_to, type, data, expected_time, groupable, status ) VALUES ( ?, ?, ?, ?, ?, ?, ? )";
+		$query = "INSERT INTO `".$wpdb->prefix."project_task` ( name, notify_to, type, data, expected_time, groupable, status ) VALUES ( %s, %d, %s, %s, %s, %d, %s )";
 		$wpdb->query($wpdb->prepare($query, $name, $target_uid, $type, $data, $expected_time, $groupable, "open"));
 	}
 
-	public function addRecord($name, $target_uid, $type, $data, $expected_time){
+	public static function updateTaskStatus($task_id, $status){
 		global $wpdb;
 
-		$query = "INSERT INTO `".$wpdb->prefix."notification` ( name, uid, type, data, expected_time ) VALUES ( ?, ?, ?, ?, ?)";
-		$wpdb->query($wpdb->prepare($query, $name, $target_uid, $type, $data, $expected_time));
-	}	
-
-	public function getTasks(){
-		global $wpdb;
-
-		$query = "SELECT * FROM `".$wpdb->prefix."task WHERE status = %s`";
-		$result = $wpdb->query($wpdb->prepare($query, "open"));
-
-		//group all groupable task for 
-		
-
-		//group all uid 
-		
-
-		return $result;
+		$query = "UPDATE `".$wpdb->prefix."project_task` SET status = %s WHERE id = %d";
+		$wpdb->query($wpdb->prepare($query, $status, $task_id));
 	}
 
-	public function getRecords($uid, $pagesize=24, $page=1){
+	public static function batchUpdateTaskStatus($task_ids, $status){
 		global $wpdb;
 
-		$query = "SELECT * FROM `".$wpdb->prefix."notification` WHERE uid = %d ORDER BY tt DESC LIMIT %d, %d";
+		if(sizeof($task_ids)){
+			$placeholders = array_fill(0, sizeof($task_ids), '%d');
+			$format = implode(", ", $placeholders);
+			$query = "UPDATE `".$wpdb->prefix."project_task` set status = %s WHERE id IN ( ".$format." )";
+
+			$params = $task_ids;
+			array_unshift($params, $status);
+			$wpdb->query($wpdb->prepare($query, $params));
+		}
+	}
+
+	public static function getActiveTasksByUID($uid){
+		global $wpdb;
+
+		$query = "SELECT * FROM `".$wpdb->prefix."project_task` WHERE notify_to = %d AND status = %s";
+		return $wpdb->get_results($wpdb->prepare($query, $uid, 'open'), ARRAY_A);
+	}
+
+	public static function getTasksByID($task_id){
+		global $wpdb;
+
+		$query = "SELECT * FROM `".$wpdb->prefix."project_task` WHERE id = %d";
+		return $wpdb->get_row($wpdb->prepare($query, $task_id), ARRAY_A);
+	}
+
+	public static function addRecord($name, $target_uid, $type, $data){
+		global $wpdb;
+
+		$query = "INSERT INTO `".$wpdb->prefix."project_notification` (  name, uid, type, data ) VALUES ( ?, ?, ?, ? )";
+		$wpdb->query($wpdb->prepare($query, $name, $target_uid, $type, $data));
+	}	
+
+	public static function getRecords($uid, $pagesize=24, $page=1){
+		global $wpdb;
+
+		$query = "SELECT * FROM `".$wpdb->prefix."project_notification` WHERE uid = %d ORDER BY tt DESC LIMIT %d, %d";
 		$data = $wpdb->get_results($wpdb->prepare($query, $uid, ($page - 1)*$pagesize, $pagesize), ARRAY_A);
 
-		$query = "SELECT COUNT(*) FROM `".$wpdb->prefix."notification` WHERE uid = %d";
+		$query = "SELECT COUNT(*) FROM `".$wpdb->prefix."project_notification` WHERE uid = %d";
 		$totalsize = $wpdb->get_var($wpdb->prepare($query, $uid));
 
 		$totalpage = ceil($totalsize / $pagesize);
